a9cd0c39c7638feaa8e30b5ada8819df53ff93ff
Alex Bayliss <bayliss_35@hotmail.com>
2022-06-08 14:54:17 +1200

    AB-changes

--- src/main/java/com/polestar/ndcrefill/FileService.java
+++ src/main/java/com/polestar/ndcrefill/FileService.java
@@ -1,16 +1,25 @@
 package com.polestar.ndcrefill;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.InvalidPropertiesFormatException;
 import java.util.List;
 import java.util.stream.Stream;
 
+@Slf4j
 public class FileService {
 
     private final String flagCode;
+
+    String DATE_REGEX = "[0-9]{4}-[0-9]{2}-[0-9]{2}";
+    String TIME_REGEX = "[0-9]{2}:[0-9]{2}:[0-9]{2}";
+
+    String SIMPLE_IMO_REGEX = "[0-9]{7}";
     int count = 0;
 
     public FileService(String flagCode) {
@@ -18,6 +27,8 @@ public class FileService {
     }
 
     public List<Position> processFile(String fileName) {
+
+        log.info("starting to process file");
         List<Position> positions = new ArrayList<>();
         Path path = Paths.get(fileName);
 
@@ -30,33 +41,71 @@ public class FileService {
                     }
                 });
             }
-            System.out.println(count);
+
         } catch (IOException e) {
+            log.error("error reading file");
             e.printStackTrace();
         }
+
+        log.info("Positions count: " + count);
 
         return positions;
     }
 
     private Position processLine(String line) {
         String[] splitted = line.split(",");
-
-        if (splitted[9].equals(flagCode)) {
-            count ++;
-            Position position = new Position();
-            position.setCreation_date(splitted[0]);
-            position.setCreation_time(splitted[1]);
-            position.setTrail_date(splitted[2]);
-            position.setTrail_time(splitted[3]);
-            position.setTrail_latitude(splitted[4]);
-            position.setTrail_longitude(splitted[5]);
-            position.setTrail_speed(splitted[6]);
-            position.setTrail_heading(splitted[7]);
-            position.setI_m_o_number(splitted[8]);
-            position.setDC_ID(splitted[9]);
-            return position;
+        try {
+            if (flagCode.equals(splitted[9])) {
+                count++;
+                Position position = new Position();
+                position.setCreation_date(splitted[0]);
+                position.setCreation_time(splitted[1]);
+                position.setTrail_date(splitted[2]);
+                position.setTrail_time(splitted[3]);
+                position.setTrail_latitude(splitted[4]);
+                position.setTrail_longitude(splitted[5]);
+                position.setTrail_speed(splitted[6]);
+                position.setTrail_heading(splitted[7]);
+                position.setI_m_o_number(splitted[8]);
+                position.setDC_ID(splitted[9]);
+                
+                validateFields(position);
+                
+                return position;
+            }
+        } catch (ArrayIndexOutOfBoundsException | InvalidPropertiesFormatException e){
+            log.error(e.getLocalizedMessage());
+            log.error("line skipped: " + line);
         }
         return null;
+    }
+
+
+
+    private void validateFields(Position position) throws InvalidPropertiesFormatException {
+        dateCheck(position.getCreation_date());
+        dateCheck(position.getTrail_date());
+        timeCheck(position.getCreation_time());
+        timeCheck(position.getTrail_time());
+        imoCheck(position.getI_m_o_number());
+    }
+
+    private void imoCheck(String i_m_o_number) throws InvalidPropertiesFormatException {
+        if(!i_m_o_number.matches(SIMPLE_IMO_REGEX)){
+            throw new InvalidPropertiesFormatException("bad i_m_o_number " + i_m_o_number);
+        }
+    }
+
+    private void timeCheck(String time) throws InvalidPropertiesFormatException {
+        if(!time.matches(TIME_REGEX)){
+            throw new InvalidPropertiesFormatException("bad time " + time);
+        }
+    }
+
+    private void dateCheck(String date) throws InvalidPropertiesFormatException {
+        if(!date.matches(DATE_REGEX)){
+            throw new InvalidPropertiesFormatException("bad date: " + date);
+        }
     }
 
 }
--- src/main/java/com/polestar/ndcrefill/Main.java
+++ src/main/java/com/polestar/ndcrefill/Main.java
@@ -1,25 +1,35 @@
 package com.polestar.ndcrefill;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.sql.*;
 import java.util.List;
 
+@Slf4j
 public class Main {
 
     private static final String HOST = "jdbc:postgresql://rds-manager.polestar-testing.local:5432/";
     private static final String MASTER_USERNAME = "test_pan_dc";
     private static final String MASTER_PASSWORD = "Vod2FrovyerUc8";
 
+    private static final boolean doInserts = true;
+
     private static final String FLAG_CODE = "1108";
-    private static int shipPositionID = 3000000;
+
+    private static final String DATA_CENTER_IDENTIFIER = "3108";
+    private static int shipPositionID = 3000050;
 
 
     public static void main(String[] args) {
 
+        log.info("starting app");
+
         Connection conn;
         try {
-
             conn = DriverManager.getConnection(HOST, MASTER_USERNAME, MASTER_PASSWORD);
             Statement statement = conn.createStatement();
+
+            getStartingDetails(statement);
 
             //get position data for DC
             FileService fileService = new FileService(FLAG_CODE);
@@ -28,25 +38,67 @@ public class Main {
             //iterate through data
             positions.forEach(position -> {
                 String insertStatement = constructSQLQuery(position);
-                System.out.println(insertStatement);
                 //execute
                 try {
-                    statement.execute(insertStatement);
+                    log.info("SQL Statement: " + insertStatement);
+                    if(doInserts){
+                        statement.execute(insertStatement);
+                    }
                 } catch (SQLException e) {
+                    log.error("Error with SQL Statement: " + insertStatement);
                     e.printStackTrace();
                 }
             });
 
+            getEndingDetails(statement);
+
         } catch (SQLException e) {
+            log.error("Error with SQL Connection?");
             e.printStackTrace();
         }
 
+        log.info("finished");
+    }
+
+    private static void getEndingDetails(Statement statement) throws SQLException {
+        log.info("Ending count in shipposition table: " + getShipPositionCount(statement));
+        log.info("new max shippositionID: " + getMaxShipPositionID(statement));
+    }
+
+    private static void getStartingDetails(Statement statement) throws SQLException {
+        log.info("Starting count in shipposition table: " + getShipPositionCount(statement));
+        log.info("ShipPosition Generator Value: " + getGeneratorValue(statement));
+
+        int maxID = getMaxShipPositionID(statement);
+        log.info("max ship position id is: " + maxID + " Current shipPositionID: " + shipPositionID);
+        if(maxID > shipPositionID){
+            throw new RuntimeException("Need to increase shipPositionID as maxID is " + maxID);
+        }
+    }
+
+    private static int getGeneratorValue(Statement statement) throws SQLException {
+        ResultSet out = statement.executeQuery("SELECT sequence_next_hi_value AS shipPositionTotal from generator where sequence_name='ShipPosition'");
+        out.next();
+        return out.getInt("shipPositionTotal");
+    }
+
+    private static int getMaxShipPositionID(Statement statement) throws SQLException {
+        //get starting count
+        ResultSet out = statement.executeQuery("SELECT max(id) AS maxID from shipposition");
+        out.next();
+        return out.getInt("maxID");
+    }
+
+    private static int getShipPositionCount(Statement statement) throws SQLException {
+        //get starting count
+        ResultSet out = statement.executeQuery("SELECT COUNT(*) AS total from shipposition");
+        out.next();
+        return out.getInt("total");
     }
 
     private static String constructSQLQuery(Position position) {
         String createTimestamp = String.format("%s %s", position.getCreation_date(), position.getCreation_time());
         String gnssTimestamp = String.format("%s %s", position.getTrail_date(), position.getTrail_time());
-        String dataCenterIdentifier = "3" + position.getDC_ID().substring(1);
 
         String sql = String.format(
                 "insert into shipposition as sp " +
@@ -54,7 +106,7 @@ public class Main {
                 "select " +
                 "'%d', '4001', '%s', '%s', '%s', '%s', '%s', '%s', (select mmsi from ship as s where s.imonumber = '%s'), '%s', '%s', '%s', (select imonumber from ship as s where s.imonumber = '%s'), 'PERIODIC_REPORT', (select shipborneequipmentidentifier from ship as s where s.imonumber = '%s'), '%s', '%s', '0', (select id from ship as s where s.imonumber = '%s') " +
                 "WHERE NOT EXISTS (SELECT id FROM shipposition WHERE shipborneequipmenttimestamp = '%s');",
-                shipPositionID, createTimestamp, createTimestamp, position.getTrail_speed(), dataCenterIdentifier, position.getDC_ID(), position.getTrail_heading(), position.getI_m_o_number(), position.getTrail_latitude(), position.getTrail_longitude(), createTimestamp, position.getI_m_o_number(), position.getI_m_o_number(), gnssTimestamp, position.getTrail_speed(), position.getI_m_o_number(), gnssTimestamp);
+                shipPositionID, createTimestamp, createTimestamp, position.getTrail_speed(), DATA_CENTER_IDENTIFIER, position.getDC_ID(), position.getTrail_heading(), position.getI_m_o_number(), position.getTrail_latitude(), position.getTrail_longitude(), createTimestamp, position.getI_m_o_number(), position.getI_m_o_number(), gnssTimestamp, position.getTrail_speed(), position.getI_m_o_number(), gnssTimestamp);
         shipPositionID++;
         return sql;
     }
